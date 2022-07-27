3a224e7c73ed033fcac5b86165bf7fe1d6e44fd1
Gustav0Futad0 <gufurtado02@gmail.com>
2022-07-27 18:48:21 -0300

    mudanca de cor e reponsividade no layout

--- js/movimentoprodutos.js
+++ js/movimentoprodutos.js
@@ -7,6 +7,7 @@ function entraSaiBarco(id) {
 
     if (elemento.parentNode == maresquerda || elemento.parentNode == mardireita) {
         //testar se bau tem elementos filho para ver se é possível entrar non barco
+        
     }
 
     if(elemento.parentNode == maresquerda && fazend.left == maresq.right) {
