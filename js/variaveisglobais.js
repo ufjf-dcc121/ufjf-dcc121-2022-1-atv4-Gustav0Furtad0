4814fba4dae432589b0293fa307b5faf502f5054
Gustav0Furtad0 <gufurtado02@gmail.com>
2022-07-28 10:27:52 -0300

    divisao de codigo js em modulos

---
+++ js/variaveisglobais.js
@@ -0,0 +1,5 @@
+const fazendeiro = document.querySelector("#fazendeiro");
+const maresquerda = document.querySelector("#maresquerda");
+const mardireita = document.querySelector("#mardireita");
+const bau = document.querySelector("#bau");
+var margemBarco = maresquerda.id;
---
+++ js/verificaWinLoss.js
@@ -0,0 +1,12 @@
+const verificaWL = () => {
+    const lobo = document.querySelector("#lobo").parentNode.id;
+    const ovelha = document.querySelector("#ovelha").parentNode.id;
+    const alface = document.querySelector("#alface").parentNode.id;
+    if (lobo == 'mardireita' && ovelha == 'mardireita' && alface == 'mardireita') {
+        console.log("Você venceu!")
+    } else if (lobo == ovelha && lobo != margemBarco) {
+        console.log("Perdeu: Lobo pegou a ovelha!!");
+    } else if (ovelha == alface && ovelha != margemBarco) {
+        console.log("Perdeu: Ovelha comeu o alface!!");
+    }
+};
