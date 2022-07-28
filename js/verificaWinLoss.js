1be97d11377b003a0ac6c6e840d1c86c00838a9e
Gustav0Furtad0 <gufurtado02@gmail.com>
2022-07-28 12:09:27 -0300

    adcionando popup ao sistema de vitoria e derrota

--- js/verificaWinLoss.js
+++ js/verificaWinLoss.js
@@ -1,20 +1,41 @@
 const verificaWL = () => {
-    
-    const popup = (tit, message) => {
-        let mensaegm = `
-        
-        `; 
+    const popup = (tit, message, btmessage) => {
+        let mensagem = `
+        <div class="mensagemfundo">
+            <div class="mensagemcorp">
+                <h3>${tit}</h3>
+                <p>${message}</p>
+                <button onclick="resmensagem()">${btmessage}</button>
+            </div>
+        </div>
+        `;
+        document.querySelector("body").innerHTML += mensagem
     }
-
 
     const lobo = document.querySelector("#lobo").parentNode.id;
     const ovelha = document.querySelector("#ovelha").parentNode.id;
     const alface = document.querySelector("#alface").parentNode.id;
     if (lobo == 'mardireita' && ovelha == 'mardireita' && alface == 'mardireita') {
-        console.log("Você venceu!")
+        popup(
+            "Você Venceu!!!",
+            "Você conseguiu deslocar os três produtos sem perdas, parabéns!",
+            "Jogar novamente?"
+        );
     } else if (lobo == ovelha && lobo != margemBarco) {
-        console.log("Perdeu: Lobo pegou a ovelha!!");
+        popup(
+            "Você Perdeu! :(",
+            "O lobo comeu a ovelha, eles ficaram sozinhos na margem...",
+            "Tentar de novo?"
+        );
     } else if (ovelha == alface && ovelha != margemBarco) {
-        console.log("Perdeu: Ovelha comeu o alface!!");
+        popup(
+            "Você Perdeu! :(",
+            "A ovelha comeu o alface, eles ficaram sozinhos na margem...",
+            "Tentar de novo?"
+        );
     }
 };
+
+const resmensagem = () => {
+    document.location.reload(true);
+}
